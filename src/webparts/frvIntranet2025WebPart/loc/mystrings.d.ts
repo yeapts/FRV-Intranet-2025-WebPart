2032e9db3722e6fde81c8ba8dfa52b01fb6465eb
yeapts <yeapts@yahoo.com>
2024-05-13 16:22:03 +1000

    Initial commit

---
+++ src/webparts/frvIntranet2025WebPart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IFrvIntranet2025WebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'FrvIntranet2025WebPartWebPartStrings' {
+  const strings: IFrvIntranet2025WebPartWebPartStrings;
+  export = strings;
+}
